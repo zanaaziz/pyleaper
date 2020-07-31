4d7f01651becafc884556c5a39004cd8dca8daad
Zana Daniel <contact@zanadaniel.com>
2020-07-31 21:56:13 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name="pyleap",
+	version="1.0.0",
+	author="Zana Daniel",
+	author_email="contact@zanadaniel.com",
+	description="A Python API for accessing your Leap Card balance, overview, and travel credit history.",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/zanadaniel/pyleap",
+	packages=setuptools.find_packages(),
+	classifiers=[
+		"Programming Language :: Python :: 3.5",
+		"License :: OSI Approved :: AGPL-3.0 License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.5.2',
+)
