ca2b791e5b47c691960041eb3d6b0038f10b4b1c
Zana Daniel <contact@zanadaniel.com>
2020-07-31 23:46:25 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
 	name="pyleaper",
-	version="1.0.0",
+	version="1.0.1",
 	author="Zana Daniel",
 	author_email="contact@zanadaniel.com",
 	description="A Python API for accessing your Leap Card balance, overview, and travel credit history.",
