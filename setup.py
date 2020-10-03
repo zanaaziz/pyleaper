24990589306e6e584797870369536a1bc129bd64
Zana Daniel <contact@zanadaniel.com>
2020-10-03 14:17:55 +0100

    v1.0.3

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
 	name="pyleapo",
-	version="1.0.2",
+	version="1.0.3",
 	author="Zana Aziz",
 	author_email="mail@zanaaziz.com",
 	description="A Python API for accessing your Leap Card balance, overview, and travel credit history.",
 	long_description=long_description,
 	long_description_content_type="text/markdown",
 	url="https://github.com/zanaaziz/pyleapo",
-	keywords="leap card leapcard api ireland",
+	keywords="leap card leapcard api ireland zana",
 	packages=setuptools.find_packages(),
 	install_requires=[
 		"Scrapy>=2.2.1",
